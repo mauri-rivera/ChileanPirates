f3cb8ee0a14aee083cf579bf05c0b5e258bcfa1f
mauri-rivera <mauri.rivera@outlook.com>
2023-12-06 09:10:18 -0300

    Update Fondo.vue

--- src/components/Fondo.vue
+++ src/components/Fondo.vue
@@ -3,18 +3,18 @@
 </script>
 
 <template>
-  <img class="fondo">
+    <img class="fondo">
 </template>
 
 <style scoped>
-  .fondo{
+.fondo {
     background-image: url("../assets/img/crucero.webp");
-    background-position: center; 
+    background-position: center;
     background-repeat: no-repeat;
     position: relative;
     width: 100vw;
     height: 100vh;
     background-size: cover;
     filter: sepia(50%) grayscale(50%);
-  }
+}
 </style>
